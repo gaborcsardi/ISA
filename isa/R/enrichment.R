0cad51858fdec2ca82d4b5d281b79de96613ca51
Gabor Csardi <Gabor.Csardi@unil.ch>
2009-01-21 19:53:14 +0100

    GO test rewritten to use the Category package with a new parameter/result type: GOListHyperGParams/GOListHyperGResult. Not all member functions are written yet, but it seems to work properly.
    The sysdata.rda file was removed, so all other enrichment calculation
    is currently broken. This will be fixed soon.

--- isa/R/enrichment.R
+++ isa/R/enrichment.R
@@ -1,4 +1,223 @@
 
+setClass("GOListHyperGParams",
+         representation(ontology="character",
+                        conditional="logical"),
+         contains="HyperGParams",
+         prototype=prototype(categoryName="GOList",
+           conditional=FALSE))
+
+setMethod("makeValidParams", "GOListHyperGParams",
+          function(object) {
+            ## TODO: proper checks
+            if (!is.list(object@geneIds)) {
+              object@geneIds <- list(object@geneIds)
+            }
+            object
+          })
+
+setMethod("ontology", "GOListHyperGParams", function(r) r@ontology)
+
+setReplaceMethod("ontology", c("GOListHyperGParams", "character"),
+                 function(r, value) {
+                     if (is.na(value) || length(value) != 1)
+                       stop("value must be a length one character vector")
+                     r@ontology <- value
+                     r
+                 })
+
+setMethod("conditional", "GOListHyperGParams", function(r) r@conditional)
+
+setReplaceMethod("conditional", c("GOListHyperGParams", "logical"),
+                 function(r, value) {
+                     if (is.na(value))
+                       stop("value must be TRUE or FALSE")
+                     r@conditional <- value
+                     r
+                 })
+
+setClass("GOListHyperGResult",
+         contains="HyperGResultBase",
+         representation=representation(
+           reslist="list",
+           conditional="logical",
+           universeGeneIds="character"),
+         prototype=prototype(
+           testName="GO",
+           reslist=list(),
+           universeGeneIds=character()))
+
+setMethod("categoryToEntrezBuilder",
+         signature(p="GOListHyperGParams"),
+         function(p) {
+           keep.all <- switch(testDirection(p),
+                              over=FALSE,
+                              under=TRUE,
+                              stop("Bad testDirection slot"))
+
+           db <- do.call(paste(p@annotation, sep="_", "dbconn"), list())
+           univ <- unlist(universeGeneIds(p), use.names=FALSE)
+
+           ## For over representation:
+           ## Obtain all unique GO IDs from specified ontology that have at
+           ## least one of the genes from geneIds(p) annotated at it.
+           ##
+           ## For under representation:
+           ## Obtain all unique GO IDs from specified ontology that have at
+           ## least one of the genes from univ annotated at it.
+           ##
+           ## These are the GO IDs that form the keys in our GO_to_Entrez map.
+           ## First we need to handle the fact that different species have different
+           ## mappings for their names.
+           if( is(p@datPkg, "YeastDatPkg") || is(p@datPkg, "Org.Sc.sgdDatPkg") ) {
+             TABLENAME = "sgd"; GENEIDS="systematic_name"
+           } else {
+             TABLENAME = "genes"; GENEIDS="gene_id"
+           }
+           SQL <- "SELECT DISTINCT go_id
+FROM %s INNER JOIN go_%s_all USING (_id)
+WHERE %s IN (%s)"
+           inClause1 <- if (!keep.all)
+             unique(unlist(geneIds(p)))
+           else
+             univ
+           inClause1 <- toSQLStringSet(inClause1) # may get reused below
+           SQL <- sprintf(SQL, TABLENAME, ontology(p), GENEIDS, inClause1)
+           wantedGO <- dbGetQuery(db, SQL)[[1]]
+           ## Now collect the Entrez IDs annotated at our wantedGO IDs making
+           ## sure to only keep those that are in the gene ID universe
+           ## specified in p.
+           SQL <- "SELECT DISTINCT %s, go_id
+FROM %s INNER JOIN go_%s_all USING (_id)
+WHERE %s IN (%s) AND go_id IN (%s)"
+           inClauseGO <- toSQLStringSet(wantedGO)
+           if (!keep.all)                      # avoid recomputing
+             inClause1 <- toSQLStringSet(univ)
+           SQL <- sprintf(SQL, GENEIDS, TABLENAME, ontology(p), GENEIDS, inClause1, 
+                          inClauseGO)
+           ans <- dbGetQuery(db, SQL)
+           if (nrow(ans) == 0)
+             list()
+           else 
+             split(ans[[GENEIDS]], ans[["go_id"]])
+         })
+
+setMethod("summary", signature(object="GOListHyperGResult"),
+          function(object, pvalue=pvalueCutoff(object), categorySize=NULL) {
+
+            if (! is.null(categorySize)) {
+              lapply(object@reslist, function(x) {
+                show <- x$Pvalue < pvalue & x$Size >= categorySize
+                x[show,]
+              })
+            } else {
+              lapply(object@reslist, function(x) {
+                show <- x$Pvalue < pvalue
+                x[show,]
+              })
+            }
+          })
+
+setMethod("htmlReport", signature=(r="GOListHyperGResult"),
+          function(r, file="", append=FALSE, label="", digits=3,
+                   summary.args=NULL) {
+            callNextMethod(r=r, file=file, append=append,
+                           label=label, digits=digits,
+                           summary.args=summary.args)            
+          })
+
+## TODO: this is the current bottleneck
+## It returns the Entrez ids from the supplied universe that
+## have at least one annotation in the supplied GO ontology
+setMethod("universeBuilder", signature=(p="GOListHyperGParams"),
+          function(p) {
+
+            datPkg <- p@datPkg
+            ontology <- ontology(p)
+            entrezIds <- universeGeneIds(p)
+            ## Return all Entrez Gene Ids that are annotated at one or more
+            ## GO terms belonging to the specified GO ontology.
+            ## If 'entrezIds' is given, return the intersection of 'entrezIds'
+            ## and the normal return value.
+            ontology <- match.arg(ontology, c("BP", "CC", "MF"))
+            
+            db <- do.call(paste(p@annotation, sep="_", "dbconn"), list())
+            if( is(p@datPkg, "YeastDatPkg") || is(p@datPkg, "Org.Sc.sgdDatPkg") ) {
+              TABLENAME = "sgd"; GENEIDS="systematic_name"
+            } else {
+              TABLENAME = "genes"; GENEIDS="gene_id"
+            }
+            CATTABLE <- paste(sep="", "go_", tolower(ontology))
+            
+            SQL <- paste(sep="", "SELECT DISTINCT ", GENEIDS,
+                         " FROM ", CATTABLE, ",", TABLENAME,
+                         " WHERE ", CATTABLE, "._id==", TABLENAME, "._id")
+            
+            probes <- dbGetQuery(db, SQL)
+            probes <- as.character(probes[,1])
+            if (length(entrezIds) != 0) {
+              probes <- intersect(probes, entrezIds)
+            }
+            probes
+          })
+
+isa.GOListHyperGTest <- function(p) {
+  p <- makeValidParams(p)
+  
+  ## Filter the universe to the genes that have at least one
+  ## annotation 
+  p@universeGeneIds <- universeBuilder(p)
+
+  ## We need the reverse mapping, the Entrez ids for all GO
+  ## categories (in this subtree).
+  gocat.ent <- as.list(categoryToEntrezBuilder(p))
+  gocat.ent <- lapply(gocat.ent, intersect, p@universeGeneIds)  
+    
+  result <- lapply(p@geneIds, function(genes) {
+    my.gocat.ent <- gocat.ent[ sapply(gocat.ent, length) != 0 ]
+    genes <- intersect(genes, p@universeGeneIds)
+    res <- .doHyperGTest(p, my.gocat.ent, list(), genes)
+    size <- sapply(my.gocat.ent, length)
+    count <- sapply(my.gocat.ent, function(x) sum(genes %in%
+                                                  x))
+    res <- data.frame(Pvalue=res$p, OddsRatio=res$odds,
+                      ExpCount=res$expected, Count=count,
+                      Size=size, row.names=names(res$p))
+    res[ order(res$Pvalue), ]
+  })
+
+  new("GOListHyperGResult",
+      reslist=result,
+      annotation=p@annotation,
+      geneIds=p@geneIds,
+      testName=categoryName(p),
+      testDirection=p@testDirection,
+      pvalueCutoff=p@pvalueCutoff,
+      conditional=p@conditional,
+      universeGeneIds=p@universeGeneIds)
+}
+
+setMethod("hyperGTest",
+          signature(p="GOListHyperGParams"), isa.GOListHyperGTest)
+
+## TODO: make this properly
+setMethod("show", signature(object="GOListHyperGResult"),
+          function(object) {
+            no.sign <- sum(sapply(object@reslist, function(x) {
+              sum(x$Pvalue < object@pvalueCutoff[1])
+            }))
+            tested <- sum(sapply(object@reslist, function(x) {
+              nrow(x)
+            }))
+            
+            cat(description(object), "\n")
+            cat(tested, testName(object), "ids tested ")
+            cat("(", no.sign, " have p < ", object@pvalueCutoff[1],
+                ")\n", sep="")
+            cat("Selected gene set size:", "TODO", "\n")
+            cat("    Gene universe size:", "TODO", "\n")
+            cat("    Annotation package:", annotation(object), "\n")
+          })
+
 isa.GO <- function(isaresult, organism=NULL, annotation=NULL, features=NULL,
                    hgCutoff=0.001, correction=TRUE) {
 
@@ -23,21 +242,15 @@ isa.GO <- function(isaresult, organism=NULL, annotation=NULL, features=NULL,
                               function(x) unlist(mget(x, ENTREZ)))
   selectedEntrezIds <- lapply(selectedEntrezIds, unique)
 
-  valid <- sapply(selectedEntrezIds, length) != 0
-
   cat(" -- Extracting Entrez Universe\n")
   entrezUniverse <- unique(unlist(mget(features, ENTREZ)))
   
   paramsBP <- paramsCC <- paramsMF <-
-    new("GOHyperGParams", geneIds = character(),
+    new("GOListHyperGParams", geneIds = selectedEntrezIds,
         universeGeneIds = entrezUniverse, annotation = annotation,
         ontology = "BP", pvalueCutoff = hgCutoff, conditional = FALSE,
         testDirection = "over")
 
-  paramsBP@geneIds <- selectedEntrezIds[valid]
-  paramsCC@geneIds <- selectedEntrezIds[valid]
-  paramsMF@geneIds <- selectedEntrezIds[valid]
-  
   ontology(paramsCC) <- "CC"
   ontology(paramsMF) <- "MF"
   
@@ -45,103 +258,15 @@ isa.GO <- function(isaresult, organism=NULL, annotation=NULL, features=NULL,
     vector(mode="list", length=length(selectedEntrezIds))
 
   cat(" -- Doing BP test\n")
-  hgOverBP[valid] <- QhyperGTest(paramsBP, correction=correction)
+  hgOverBP <- hyperGTest(paramsBP)
   cat(" -- Doing CC test\n")
-  hgOverCC[valid] <- QhyperGTest(paramsCC, correction=correction)
+  hgOverCC <- hyperGTest(paramsCC)
   cat(" -- Doing MF test\n")
-  hgOverMF[valid] <- QhyperGTest(paramsMF, correction=correction)
-  
-  hgOverBP[!valid] <- hgOverCC[!valid] <- hgOverMF[!valid] <- NA
+  hgOverMF <- hyperGTest(paramsMF)
   
   list(hgOverBP, hgOverCC, hgOverMF)
 }
   
-QhyperGTest <- function(params, correction) {
-
-  ## These are not yet implemented
-  if (params@testDirection == "under") {
-    stop("Under-representation is not implemented yet")
-  }
-  if (params@conditional) {
-    stop("Conditional tests are not implemented yet")
-  }
-  
-  ## Check that we have the require Entrez <-> GO mappings,
-  pkg <- sub(".db", "", params@datPkg@name, fixed=TRUE)
-  organism <- get(paste(sep="", pkg, "ORGANISM"))
-  organism <- abbreviate(organism, 2)
-
-##   maps <- paste(sep=".", "org", organism,
-##                 c("egGO", "egGO2ALLEGS", "egGOALLCC", "egGOALLBP", "egGOALLMF",
-##                   "egGOHAVECC", "egGOHAVEBP", "egGOHAVEMF"))
-##   if (any(! sapply(maps, exists))) {
-##     stop("Some Entrez <-> GO mappings do not exist: ", sapply(maps,exists))
-##   }
-
-  ## Filter the gene universe to include genes that have at least one annotation
-  ## in the current ontology
-  havelist <- get(paste(sep="", "org.", organism, ".egGOHAVE", ontology(params)))
-  if (length(params@universeGeneIds)==0) {
-    params@univerGeneIds <- havelist
-  } else { 
-    params@universeGeneIds <- intersect(havelist, params@universeGeneIds)
-  }
-
-  ## Pre-generate the filtered list of probes, only include those that are
-  ## in the universe
-  if (!is.list(params@geneIds)) {
-    params@geneIds <-list(params@geneIds)
-  }
-  all.egs <- get(paste(sep=".", "org", organism, "egGO2ALLEGS"))
-  if (length(params@geneIds) > 3) {
-    simplified <- TRUE
-    egGO2ALLEGS.mine <- new.env()
-    all <- as.list(all.egs)
-    all <- lapply(all, function(x) intersect(x, params@universeGeneIds))
-    for (n in seq(all)) {
-      assign(names(all)[n], all[[n]], envir=egGO2ALLEGS.mine)
-    }
-  } else {
-    simplified <- FALSE
-    egGO2ALLEGS.mine <- all.egs
-  }
-
-  ## Ok, start testing
-  all.cc <- get(paste(sep="", "org.", organism, ".egGOALL", ontology(params)))
-      
-  result <- lapply(params@geneIds, function(genes) {
-
-    genes <- as.character(genes[!is.na(genes)])
-    tmp <- mget(genes, all.cc, ifnotfound=NA)
-    tmp <- tmp[ !is.na(tmp) ]
-    
-    tmp2 <- unique(as.character(unlist(unname(tmp))))
-    tmp5 <- mget(tmp2, egGO2ALLEGS.mine)
-    if (!simplified) {
-      tmp5 <- lapply(tmp5, function(x) intersect(x, params@universeGeneIds))
-    }
-
-    selected <- genes[ genes %in% params@universeGeneIds ]
-    res <- .doHyperGTest(params, tmp5, list(), selected)
-    res$size <- sapply(tmp5, length)
-    res$count <- sapply(tmp5, function(x) sum(selected %in% x))
-    drive <- lapply(tmp5, function(x) genes[which(genes %in% x)])
-    res$drive <- sapply(drive, paste, collapse=";")
-
-    if (correction && length(res$p)>0) {
-      res$p <- res$p * length(res$size)
-      res$p <- pmin(res$p, 1)
-    }
-
-    res <- data.frame(Pvalue=res$p, OddsRatio=res$odds, ExpCount=res$expected,
-                      Count=res$count, Size=res$size, Drive=res$drive,
-                      row.names=names(res$p))
-    res[ order(res$Pvalue), ]
-  })
-
-  result
-}
-
 isa.KEGG <- function(isaresult, organism=NULL, annotation=NULL, features=NULL,
                      hgCutoff=0.001, correction=TRUE) {
 
