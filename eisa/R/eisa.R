276bc9d182c6a8862ae008b0f5348739c2f65cc9
Gabor Csardi <Gabor.Csardi@unil.ch>
2009-03-30 16:51:09 +0200

    eisa() filters genes, by default for IQR>0.5.

--- eisa/R/eisa.R
+++ eisa/R/eisa.R
@@ -1,11 +1,18 @@
 
-eisa <- function(exp.set, thr.gene=seq(2,4,by=0.5), thr.cond=seq(1,3,by=0.5),
+eisa <- function(exp.set, flist=filterfun(function(x) IQR(x) > 0.5),
+                 thr.gene=seq(2,4,by=0.5), thr.cond=seq(1,3,by=0.5),
                  no.seeds=100) {
-
+  
   if (!is(exp.set, "ExpressionSet")) {
     stop("Please supply an ExpressionSet object")
   }
 
+  if (!is.function(flist) && !is.na(flist)) {
+    require(genefilter)
+    selected <- genefilter(exp.set, flist)
+    exp.set <- exp.set[selected,]
+  }
+  
   isares <- isa(exprs(exp.set), thr.row=thr.gene, thr.col=thr.cond,
                 no.seeds=no.seeds)
 
