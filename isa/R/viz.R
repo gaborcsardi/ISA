f3b9d4aad3ef837f7a853918e2ba3f41ca4ea8f7
Gabor Csardi <Gabor.Csardi@unil.ch>
2009-04-29 14:42:21 +0200

    isa: added plot.modules to quickly create image plots of modules.

---
+++ isa/R/viz.R
@@ -0,0 +1,49 @@
+
+plot.modules <- function(modules, to.plot=seq_len(ncol(modules$rows)),
+                         data, binary=TRUE, names=NULL, xlab="", ylab="",
+                         ...) {
+
+  require(lattice)
+  
+  no.mods <- ncol(modules$rows)
+  no.rows <- nrow(modules$rows)
+  no.cols <- nrow(modules$columns)
+  
+  if (no.mods==0) {
+    stop("No modules")
+  }
+
+  if (!missing(data)) {
+    if (nrow(data) != no.rows || ncol(data) != no.cols) {
+      stop("`data' has a different dimension than `modules'")
+    }
+  }
+  
+  M <- lapply(seq_len(no.mods), function(x) {
+    if (binary) {
+      outer(modules$rows[,x]!=0, modules$columns[,x]!=0)
+    } else {
+      outer(modules$rows[,x], modules$columns[,x])
+    }
+  })  
+  
+  if (!missing(data)) {
+    no.mods <- no.mods + 1
+    M <- c(list(data), M)
+  }
+
+  alldata <- structure(unlist(M), dim=c(no.rows, no.cols, no.mods),
+                       dimnames=list(NULL, NULL, names))
+
+  rx <- range(alldata, finite=TRUE)
+  nn <- 100
+  n0 <- min(nn, max(0, round((0-rx[1])/(rx[2]-rx[1])*nn)))
+  col.regions <- c(colorRampPalette(c("blue3", "gray80"))(n0),
+                   colorRampPalette(c("gray75", "red3"))(nn-n0))
+
+  levelplot(alldata, scale=list(y=list(draw=FALSE), x=list(draw=FALSE)),
+            col.regions=col.regions, between=list(x=0.5,y=0.5),
+            xlab=xlab, ylab=ylab, ...)
+}
+
+                       
