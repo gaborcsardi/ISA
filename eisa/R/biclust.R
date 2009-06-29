872d9e802d25e123db43aafe9d2383d35ccf6af3
Gabor Csardi <Gabor.Csardi@unil.ch>
2009-06-29 20:14:13 +0200

    eisa: added eisa.biclust method.

---
+++ eisa/R/biclust.R
@@ -0,0 +1,12 @@
+
+eisa.biclust <- function(modules) {
+
+  if (!require(biclust)) {
+    stop("The `biclust' package is required for this")
+  }
+
+  new("Biclust", Parameters=list(seeddata=modules@seeddata,
+                   rundata=modules@rundata),
+      RowxNumber=modules@genes != 0,
+      NumberxCol=t(modules@conditions != 0), Number=ncol(modules@genes))
+}
