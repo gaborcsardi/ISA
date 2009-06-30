6b7c69f59491376edf0ec14df663f6630fbf5d1a
Gabor Csardi <Gabor.Csardi@unil.ch>
2009-06-30 14:55:43 +0200

    isa2: prenormalize by default.

--- isa2/R/isa3.R
+++ isa2/R/isa3.R
@@ -2,7 +2,7 @@
 setMethod("isa.normalize", signature(data="matrix"),
           function(data, ...) isa.normalize.default(data, ...))
 
-isa.normalize.default <- function(data, prenormalize=FALSE) {
+isa.normalize.default <- function(data, prenormalize=TRUE) {
 
   isa.status("ISA normalization", "in")
 
