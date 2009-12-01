8b2e18655ae522d5d182b0383c44ad8ecf6be2d9
Andreas Lüscher <andreas@maya>
2009-12-01 13:16:56 +0100

    RExpressionView: Updated documentation.

--- RExpressionView/R/launch.R
+++ RExpressionView/R/launch.R
@@ -1,5 +1,5 @@
 LaunchEV <- function() {
-	swf <- system.file("ExpressionView.html", package="ExpressionView")
-	url <- URLencode(paste("file://", swf, sep=""))
-	browseURL(url)
+    swf <- system.file("ExpressionView.html", package="ExpressionView")
+    url <- URLencode(paste("file://", swf, sep=""))
+    browseURL(url)
 }
