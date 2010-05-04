be344a56278f943c4e08d87ae07030a6d3e267b7
Gabor Csardi <csardi.gabor@gmail.com>
2010-05-04 16:18:09 +0200

    Make the biclust <-> ISA conversion work properly.

--- eisa/R/biclust.R
+++ eisa/R/biclust.R
@@ -1,39 +1,35 @@
 
-if (require(biclust, quietly=TRUE)) {
+setAs(from="Biclust", to="ISAModules", def=function(from) {
 
-  setAs(from="Biclust", to="ISAModules", def=function(from) {
-
-    feat <- rownames(from@Parameters$Data)
-    samp <- colnames(from@Parameters$Data)
-    anno <- from@Parameters$annotation
-    
-    if (is.null(feat)) {
-      warning("No feature names in `Biclust' object")
-    }
-    if (is.null(samp)) {
-      warning("No sample names in `Biclust' object")
-    }
-    if (is.null(anno)) {
-      warning("No `annotation' in `Biclust' object")
-    }
-    if (is.null(from@Parameters$pData) && !is.null(samp)) {
-      from@Parameters$pData <- data.frame(row.names=samp)
-    }
-    
-    new("ISAModules",
-        genes=structure(from@RowxNumber, dimnames=list(feat, NULL)),
-        conditions=structure(t(from@NumberxCol), dimnames=list(samp, NULL)),
-        rundata=from@Parameters,
-        seeddata=data.frame())
-  })
+  feat <- rownames(from@Parameters$Data)
+  samp <- colnames(from@Parameters$Data)
+  anno <- from@Parameters$annotation
   
-  setAs(from="ISAModules", to="Biclust", def=function(from) {
-    library(biclust)
-    new("Biclust", Parameters=list(seeddata=from@seeddata,
-                     rundata=from@rundata),
-        RowxNumber=from@genes != 0,
-        NumberxCol=t(from@conditions != 0), Number=ncol(from@genes))
-  })
+  if (is.null(feat)) {
+    warning("No feature names in `Biclust' object")
+  }
+  if (is.null(samp)) {
+    warning("No sample names in `Biclust' object")
+  }
+  if (is.null(anno)) {
+    warning("No `annotation' in `Biclust' object")
+  }
+  if (is.null(from@Parameters$pData) && !is.null(samp)) {
+    from@Parameters$pData <- data.frame(row.names=samp)
+  }
+  
+  new("ISAModules",
+      genes=structure(from@RowxNumber, dimnames=list(feat, NULL)),
+      conditions=structure(t(from@NumberxCol), dimnames=list(samp, NULL)),
+      rundata=from@Parameters,
+      seeddata=data.frame())
+})
 
-}
+setAs(from="ISAModules", to="Biclust", def=function(from) {
+  library(biclust)
+  new("Biclust", Parameters=list(seeddata=from@seeddata,
+                   rundata=from@rundata),
+      RowxNumber=from@genes != 0,
+      NumberxCol=t(from@conditions != 0), Number=ncol(from@genes))
+})
 
