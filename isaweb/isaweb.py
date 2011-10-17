b3c632d293c1e44a63a1779bd6afb496c526fcac
Gabor Csardi <csardi.gabor@gmail.com>
2011-10-17 16:17:31 +0200

    isaweb improvements.

--- isaweb/isaweb.py
+++ isaweb/isaweb.py
@@ -5,12 +5,17 @@ import model
 import re
 import odict
 
+web.config.debug = False
+
 ### URL mappings
 
 urls = (
-    '/?',                                            'Index',
-    '/search/?',                                     'Search',
-    '/search/(\w+)/(\w+)/([ \w+\-\*]+)/(\d+)/(\d+)', 'Search',
+    '/search/?',                                             'Search',
+    '/search/(\w+)/(\w+)/([ \w\-\*]*)/(\d+)/(\d+)/([123])',  'Search',
+    '/modules/(\w+)/(\w+)/([ \w\-\*]*)/([123])',             'Modules',
+    '/help/?',                                               'Help',
+    '/paper/?',                                              'Paper',
+    '.*',                                                    'Index'
     )
 
 app=web.application(urls, globals())
@@ -27,13 +32,21 @@ def unique(seq):
         keys[e] = 1
     return keys.keys()
 
+def modulelink(id):
+    id=str(id)
+    if len(id) > 0 and id[0]=='p':
+        return globals['isahtmlpath2'] + 'module-' + id[1:] + '.html'
+    else:
+        return globals['isahtmlpath'] + 'module-' + id + '.html'
+
 ### Templates
 
 globals = { 'sub': re.sub,
+            'modulelink': modulelink,
             'home': '',
             'static': '/cbg/ISA/species/static',
-            'isahtmlpath': 'http://www2.unil.ch/cbg/ISA/species/isa8-html2/',
-            'isahtmlpath2': 'http://www2.unil.ch/cbg/ISA/species/isa10-html/', 
+            'isahtmlpath': 'http://www2.unil.ch/cbg/ISA/species/isa8-html4/',
+            'isahtmlpath2': 'http://www2.unil.ch/cbg/ISA/species/isa10-html2/', 
             'ensemblurl' : 'http://www.ensembl.org/%s/geneview?gene=%s',
             'entrezurl': 'http://www.ncbi.nlm.nih.gov/entrez/query.fcgi?db=gene&cmd=Retrieve&dopt=full_report&list_uids=%s',
             'gourl': 'http://amigo.geneontology.org/cgi-bin/amigo/go.cgi?view=details&search_constraint=terms&depth=0&query=%s',
@@ -53,18 +66,33 @@ class Index:
 
 class Search:
 
+    def filt(self, res, which='3'):
+        if which=='3':
+            return res
+        for i in range(len(res)):
+            if res[i].modules == '':
+                continue
+            sp = res[i].modules.split(';')            
+            if which=='1':
+                sp = [s for s in sp if s[0] != 'p']
+            else:
+                sp = [s for s in sp if s[0] == 'p']
+            res[i].modules = ";".join(sp)
+        return res
+
     def get_modules(self, modstrings=''):
         modlist=[ r.split(";") for r in modstrings if r != '']
         mods=[ i for s in modlist for i in s ] # flatten
-        mods=model.get_modules(unique(mods))
+        mods=model.get_modules(unique(mods))        
         modules={}
         for m in mods:
             modules[str(m.id)]=m
         return modules
 
-    def search_for_gene(self, field, key, limit, offset):
+    def search_for_gene(self, field, key, which, limit, offset):
         res=model.search_for_gene(field, key, limit, offset)
-        modules=self.get_modules(modstrings=[ r.modules for r in res ])
+        res=self.filt(res, which)
+        modules=self.get_modules([ r.modules for r in res ])
         return render_plain.search_gene(res, modules)
 
     def get_pvals(self, res, modules):
@@ -78,28 +106,30 @@ class Search:
             enr[r.id] = getp(model.get_enrichment(r.id))
         return enr
 
-    def search_for_enrichment(self, field, key, limit, offset):
-        res=model.search_for_enrichment(field, key, limit, offset)
-        modules=self.get_modules(modstrings=[ r.modules for r in res ])
+    def search_for_enrichment(self, field, key, which, limit, offset):
+        res=model.search_for_enrichment(field, key, limit, offset, which)
+        res=self.filt(res, which)
+        modules=self.get_modules([ r.modules for r in res ])
         pvals=self.get_pvals(res, modules)
         return render_plain.search_enrichment(res, modules, pvals)
 
-    def search_for_species(self, field, key, limit, offset):
+    def search_for_species(self, field, key, which, limit, offset):
         res=model.search_for_species(field, key, limit, offset)
         spec=[s.id for s in res]
-        mods=model.get_modules_for_species(spec)
+        mods=model.get_modules_for_species(spec, which)
         for n in range(len(res)):
             res[n].modules=mods[n]
-        modules=self.get_modules(modstrings=mods)
+        modules=self.get_modules(mods)
         return render_plain.search_species(res, modules)
 
-    def search_for_tissue(self, field, key, limit, offset):
+    def search_for_tissue(self, field, key, which, limit, offset):
         res=model.search_for_tissue(field, key, limit, offset)
+        print res
         tiss=[t.id for t in res]
-        mods=model.get_modules_for_tissues(tiss)
+        mods=model.get_modules_for_tissues(tiss, which)
         for n in range(len(res)):
             res[n].modules=mods[n]
-        modules=self.get_modules(modstrings=mods)
+        modules=self.get_modules(mods)
         return render_plain.search_tissue(res, modules)
 
     def get_pos(self, hitcounts, offset, limit):
@@ -123,9 +153,11 @@ class Search:
     def get_search_expression(self, what, field, key, limit, offset):
         return key
 
-    def GET(self, what="any", field="any", key="*", limit=10, offset=0):
+    def GET(self, what="any", field="any", key="*", limit=10, offset=0, 
+            which='3'):
+
         if what not in ("any", "gene", "enrichment", "species", "tissue"):
-            return web.notfound()
+            return web.notfound()        
 
         sexp=self.get_search_expression(what, field, key, limit, offset)
 
@@ -141,16 +173,16 @@ class Search:
 
         ## Get the hit counts
         hitcounts['species']= \
-            model.search_for_species(field, key, count=True)[0].count
+            model.search_for_species(field, key, which, count=True)[0].count
         chitcounts['species']=0
         hitcounts['tissue']=\
-            model.search_for_tissue(field, key, count=True)[0].count
+            model.search_for_tissue(field, key, which, count=True)[0].count
         chitcounts['tissue']=hitcounts['species']
         hitcounts['gene']= \
-            model.search_for_gene(field, key, count=True)[0].count
+            model.search_for_gene(field, key, which, count=True)[0].count
         chitcounts['gene']=chitcounts['tissue']+hitcounts['tissue'];
         hitcounts['enrichment']= \
-            model.search_for_enrichment(field, key, count=True)[0].count
+            model.search_for_enrichment(field, key, which,count=True)[0].count
         chitcounts['enrichment']=chitcounts['gene']+hitcounts['gene']
 
         ## What to show
@@ -158,30 +190,83 @@ class Search:
 
         ## Show it
         if what in ('species', 'any') and limits['species'] != 0:
-            html['species']=self.search_for_species(field, key, 
+            html['species']=self.search_for_species(field, key, which,
                                                     limits['species'],
                                                     offsets['species'])
         if what in ('tissue', 'any') and limits['tissue'] != 0:
-            html['tissue']=self.search_for_tissue(field, key, 
+            html['tissue']=self.search_for_tissue(field, key, which,
                                                   limits['tissue'],
                                                   offsets['species'])
         if what in ('gene', 'any') and limits['gene'] != 0:
-            html['gene']=self.search_for_gene(field, key, limits['gene'], 
+            html['gene']=self.search_for_gene(field, key, which,
+                                              limits['gene'], 
                                               offsets['gene'])
         if what in ('enrichment', 'any') and limits['enrichment'] != 0:
             html['enrichment']= \
-                self.search_for_enrichment(field, key, limits['enrichment'], 
+                self.search_for_enrichment(field, key, which,
+                                           limits['enrichment'], 
                                            offsets['enrichment'])
 
         return render.searchresults(hitcounts, chitcounts, 
                                     html, offset, web.ctx.home,
                                     field, key, limit, what,
                                     sum(hitcounts.values()), 
-                                    sexp)
+                                    sexp, which)
 
     def POST(self):
         inp=web.input()
-        web.seeother("/search/any/any/*" + inp.key.strip("*") + "*/10/0")
+
+        which=0
+        if 'complete' in inp and inp.complete=='on':
+            which=which+1
+        if 'primate' in inp and inp.primate=='on':
+            which=which+2
+
+        if re.search("Search", inp.action):
+            web.seeother("/search/any/any/" + inp.key + "/10/0/" + str(which))
+        else:
+            web.seeother("/modules/any/any/" + inp.key + "/" + str(which))
+
+class Modules:
+
+    def get_search_expression(self, what, field, key):
+        return key
+
+    def get_modules(self, modules):
+        mods=model.get_modules(modules.keys())
+        for m in mods:
+            m["comment"]=modules[str(m.id)]
+        return mods
+
+    def GET(self, what="any", field="any", key="*", which='3'):
+
+        sexp=self.get_search_expression(what, field, key)
+
+        mods={}
+        if what in ('species', 'any'):
+            mods=model.modules_for_species(field, key, mods, which)
+        if what in ('tissue', 'any'):
+            mods=model.modules_for_tissue(field, key, mods, which)
+        if what in ('gene', 'any'):
+            mods=model.modules_for_gene(field, key, mods, which)
+        if what in ('enrichment', 'any'):
+            mods=model.modules_for_enrichment(field, key, mods, which)
         
+        mods=self.get_modules(mods)
+        mods.sort(key=lambda x: (-x.comment.count(";"), x.comment, 
+                                  x.species, x.tissues))
+
+        return render.modules(mods, sexp, which)
+
+class Help:
+
+    def GET(self):
+        return render.help()
+
+class Paper:
+    
+    def GET(self):
+        return render.paper()
+
 app.run()
 
--- isaweb/model.py
+++ isaweb/model.py
@@ -1,23 +1,53 @@
 import web
+import re
+
+def unique(seq):
+    keys = {}
+    for e in seq:
+        keys[e] = 1
+    return keys.keys()
+
+def unique2(seqx):
+    modlist=[ r.split(";") for r in seq if r != '']
+    mods=[ i for s in modlist for i in s ] # flatten
+    return unique(mods)    
 
 db = web.database(dbn='sqlite', db='modules.db')
 
-def search_for_species(field, key, limit=None, offset=None, count=False):
+def regexp(expr, item):
+    return int(re.search(expr, item, re.IGNORECASE) is not None)
 
-    if '*' in key:
-        key=key.replace("*", "%")
-        if field=="any":
-            key = (web.safestr(key),) * 3
-            where = ("id LIKE '%s' OR name LIKE '%s' " +
-                     "OR sciname LIKE '%s'") % key
-        else:
-            where = "%s LIKE '%s'" % (web.safestr(field), web.safestr(key))
+def install_regexp():
+    db._getctx().db.create_function('REGEXP', 2, regexp)
+
+def where_clause(fields, key, words=True):
+    if '%' in key:
+        key = (web.websafe(key),) * len(fields)
+        clause = "%s LIKE '%s'"
+        where = " OR ".join([ clause %(f,k) for f,k in zip(fields, key)])
     else:
-        if field=="any":
-            key = (web.safestr(key),) * 3
-            where = "id='%s' OR name='%s' OR sciname='%s'" % key
+        if words:
+            install_regexp()
+            clause="%s REGEXP '\\b%s\\b'"
         else:
-            where = "%s='%s'" % (web.safestr(field), web.safestr(key))
+            clause="%s = '%s'"
+        key = (web.websafe(key),) * len(fields)
+        where=" OR ".join([ clause % (f,k) for f, k in zip(fields, key)])
+        
+    return "(" + where + ")"
+
+def multi_search(field, fields, keys, combine, words):
+    keys=keys.replace("*", "%").split()
+    if field=='any':
+        field=fields
+    where=combine.join(where_clause(field, key, words=words) for key in keys)
+    return where
+
+def search_for_species(field, keys, limit=None, offset=None, 
+                       words=True, count=False, combine=" OR "):
+
+    where=multi_search(field, ('id', 'name', 'sciname'), keys,
+                       combine, words=words)
     
     if count: 
         what="count(*) as count"
@@ -28,36 +58,42 @@ def search_for_species(field, key, limit=None, offset=None, count=False):
 
     res=db.select('species', what=what, where=where, 
                   limit=limit, offset=offset)
-    return [r for r in res]
+    return list(res)
 
-def get_modules_for_species(spec):
+def get_modules_for_species(spec, which='3'):
+
+    if which=='1':
+        where="id NOT LIKE 'p%' AND "
+    elif which=='2':
+        where="id LIKE 'p%' AND "
+    else:
+        where=""
+
     def get_mod(s):
-        res=db.select('module', what="id", where="species LIKE '%%%s%%'" % s)
+        res=db.select('module', what="id", 
+                      where=where + ("species LIKE '%%%s%%'" % s))
         return ";".join([str(r.id) for r in res])
     return [get_mod(s) for s in spec]
 
-def get_modules_for_tissues(tiss):
+def get_modules_for_tissues(tiss, which='3'):
+
+    if which=='1':
+        where="id NOT LIKE 'p%%' AND tissues LIKE '%%%s%%'"
+    elif which=='2':
+        where="id LIKE 'p%%' AND tissues LIKE '%%%s%%'"
+    else:
+        where="tissues LIKE '%%%s%%'"
+
     def get_mod(s):
-        res=db.select('module', what="id", where="tissues LIKE '%%%s%%'" % s)
+        res=db.select('module', what="id", where=where % s)
         return ";".join([str(r.id) for r in res])
     return [get_mod(s) for s in tiss]
 
-def search_for_tissue(field, key, limit=None, offset=None, count=False):
+def search_for_tissue(field, keys, limit=None, offset=None, 
+                      words=True, count=False, combine=" OR "):
 
-    if '*' in key:
-        key=key.replace("*", "%")
-        if field=="any":
-            key = (web.safestr(key),) * 2
-            where = ("id LIKE '%s' OR name LIKE '%s' ") % key
-        else:
-            where = "%s LIKE '%s'" % (web.safestr(field), web.safestr(key))
-    else:
-        if field=="any":
-            key = (web.safestr(key),) * 3
-            where = "id='%s' OR name='%s' " % key
-        else:
-            where = "%s='%s'" % (web.safestr(field), web.safestr(key))
-    
+    where=multi_search(field, ('id', 'name'), keys, combine, words=words)
+
     if count: 
         what="count(*) as count"
         limit=None
@@ -67,26 +103,14 @@ def search_for_tissue(field, key, limit=None, offset=None, count=False):
 
     res=db.select('tissue', what=what, where=where, 
                   limit=limit, offset=offset)
-    return [r for r in res]
+    return list(res)
     
+def search_for_gene(field, keys, limit=None, offset=None, words=True, 
+                    count=False, combine=" OR "):
 
-def search_for_gene(field, key, limit=None, offset=None, count=False):
+    where=multi_search(field, ('ensembl', 'entrez', 'symbol', 'name'), keys,
+                       combine, words=words)
 
-    if '*' in key:
-        key=key.replace("*", "%")
-        if field=="any":
-            key = (web.safestr(key),) * 4
-            where = ("ensembl LIKE '%s' OR entrez LIKE '%s' " +
-                     "OR symbol LIKE '%s' OR name LIKE '%s'") % key
-        else:
-            where = "%s LIKE '%s'" % (web.safestr(field), web.safestr(key))
-    else:
-        if field=="any":
-            key = (web.safestr(key),) * 4
-            where = "ensembl='%s' OR entrez='%s' OR symbol='%s' OR name='%s'" % key
-        else:
-            where = "%s='%s'" % (web.safestr(field), web.safestr(key))
-    
     if count: 
         what="count(*) as count"
         limit=None
@@ -98,25 +122,12 @@ def search_for_gene(field, key, limit=None, offset=None, count=False):
 
     res=db.select('gene', what=what, where=where, 
                   limit=limit, offset=offset, order=order)
-    return [r for r in res]
+    return list(res)
 
-def search_for_enrichment(field, key, limit=None, offset=None, count=False):
+def search_for_enrichment(field, keys, limit=None, offset=None, 
+                          words=True, count=False, combine=" OR "):
 
-    if '*' in key:
-        key=key.replace("*", "%")
-        if field=="any":
-            key = (web.safestr(key),) * 2
-            where = "id LIKE '%s' OR name LIKE '%s'" % key
-        else:
-            where = "type='%s' AND (id LIKE '%s' OR name LIKE '%s')" % \
-                (web.safestr(field), web.safestr(key), web.safestr(key))
-    else:
-        if field=="any":
-            key = (web.safestr(key),) * 2
-            where = "id='%s' OR name='%s'" % key
-        else:
-            where = "type='%s' AND (id='%s' OR name='%s')" % \
-                (web.safestr(field), web.safestr(key), web.safestr(key))
+    where=multi_search(field, ('id', 'name'), keys, combine, words=words)
 
     if count: 
         what="count(*) as count"
@@ -129,7 +140,7 @@ def search_for_enrichment(field, key, limit=None, offset=None, count=False):
 
     res=db.select('category', what=what, where=where, 
                   limit=limit, offset=offset, order=order)
-    return [r for r in res]
+    return list(res)
 
 def get_module_types():
 
@@ -137,11 +148,93 @@ def get_module_types():
 
 def get_modules(mods):
     res=db.select('module', where='id in (' + 
-                  ",".join([str(m) for m in mods]) + ')')
-    return [r for r in res]
+                  ",".join(["'%s'" % str(m) for m in mods]) + ')')
+    return list(res)
 
 def get_module(m):
-    return db.select('module', where="id=%s" % int(m))
+    return db.select('module', where="id='%s'" % m)
 
 def get_enrichment(c):
     return db.select('enrichment', where="category='%s'" % c)
+
+def modules_for_species(field, keys, mods, which, words=True, combine=" OR "):
+    where=multi_search(field, ('id', 'name', 'sciname'), keys, combine,
+                       words=words)
+    spec=[s.id for s in db.select('species', what='id', where=where)]
+    spec=unique(spec)
+    if len(spec)==0:
+        return mods
+    where=" OR ".join("species LIKE '%%%s%%'" % s for s in spec)
+    if which=='1':
+        where="id NOT LIKE 'p%' AND (" + where + ")"
+    elif which=='2':
+        where="id LIKE 'p%' AND (" + where + ")"
+    specmods=db.select('module', what='id,species', where=where)
+    for rec in specmods:
+        m=str(rec.id)
+        for s in rec.species.split(";"):
+            s2=s.split(":")[1]
+            if s2 in spec:
+                if m in mods:
+                    mods[m]=mods[m] + '; <span class="s">%s</span>' % s2
+                else:
+                    mods[m]='<span class="s">%s</span>' % s2
+    return mods
+
+def modules_for_tissue(field, keys, mods, which, words=True, combine=" OR "):
+    where=multi_search(field, ('id', 'name'), keys, combine, words=words)
+    tiss=[t.id for t in db.select('tissue', what='id', where=where)]
+    tiss=unique(tiss)
+    if len(tiss)==0:
+        return mods
+    where=" OR ".join("tissues LIKE '%%%s%%'" % t for t in tiss)
+    if which=='1':
+        where="id NOT LIKE 'p%' AND (" + where + ")"
+    elif which=='2':
+        where="id LIKE 'p%' AND (" + where + ")"
+    tissmods=db.select('module', what='id,tissues', where=where)
+    for rec in tissmods:
+        m=str(rec.id)
+        for t in rec.tissues.split(";"):
+            t2=t.split(":")[1]
+            if t2 in tiss:
+                if m in mods:
+                    mods[m]=mods[m] + '; <span class="t">%s</span>' % t2
+                else:
+                    mods[m]='<span class="t">%s</span>' % t2
+    return mods
+
+def modules_for_gene(field, keys, mods, which, words=True, combine=" OR "):
+    where=multi_search(field, ("ensembl", "entrez", "symbol", "name"), 
+                       keys, combine, words=words)
+    genes=db.select("gene", what='modules,symbol', where=where)
+    for rec in genes:
+        if rec.modules=='':
+            continue;
+        for m in rec.modules.split(";"):
+            if (which=='1' and m[0]=='p') or (which=='2' and m[0]!='p'):
+                continue
+            s=rec.symbol.replace(";", ", ")
+            if m in mods:
+                mods[m]=mods[m] + '; <span class="g">%s</span>' % s
+            else:
+                mods[m]='<span class="g">%s</span>' % s
+    return mods
+
+def modules_for_enrichment(field, keys, mods, which, words=True,
+                           combine=" OR "):
+    where=multi_search(field, ('id', 'name'), keys, combine, words=words)
+    cats=db.select('category', what="modules,name", where=where)
+    for rec in cats:
+        if rec.modules=='':
+            continue;
+        for m in rec.modules.split(";"):
+            if (which=='1' and m[0]=='p') or (which=='2' and m[0]!='p'):
+                continue
+            n=rec.name
+            if m in mods:
+                mods[m]=mods[m] + '; <span class="e">%s</span>' % n
+            else:
+                mods[m]='<span class="e">%s</span>' % n
+    return mods
+
