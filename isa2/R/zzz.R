9c958716580da99bcdcec862b54ddf623933b226
Gábor Csárdi <csardi.gabor@gmail.com>
2017-03-01 14:15:04 +0000

    isa2: no need for explicit library.dynam

--- isa2/R/zzz.R
+++ isa2/R/zzz.R
@@ -1,6 +1,4 @@
 .onLoad <- function(lib, pkg) {
-  library.dynam("isa2", pkg, lib, local=FALSE);
-
   ## Default ISA options
   isa.options[["verbose"]] <-  FALSE
   isa.options[["status.function"]] <- function(...) isa.status.default(...)
